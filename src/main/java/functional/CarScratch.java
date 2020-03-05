3f79c8cd56cc13dd4ed402fbe2d2ee281e98a949
招文桃 <gdouzwt@gmail.com>
2020-03-06 04:52:28 +0800

    完成简单的问题引入

--- src/main/java/functional/CarScratch.java
+++ src/main/java/functional/CarScratch.java
@@ -1,24 +1,38 @@
 package functional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CarScratch {
-  public static void showAll(List<Car> lc) {
-    for (Car c : lc) {
-      System.out.println(c);
+    public static void showAll(List<Car> lc) {
+        for (Car c : lc) {
+            System.out.println(c);
+        }
+        System.out.println("-------------------------------------");
     }
-    System.out.println("-------------------------------------");
-  }
-  
-  public static void main(String[] args) {
-    List<Car> cars = Arrays.asList(
-        Car.withGasColorPassengers(6, "Red", "Fred", "Jim", "Sheila"),
-        Car.withGasColorPassengers(3, "Octarine", "Rincewind", "Ridcully"),
-        Car.withGasColorPassengers(9, "Black", "Weatherwax", "Magrat"),
-        Car.withGasColorPassengers(7, "Green", "Valentine", "Gillian", "Anne", "Dr. Mahmoud"),
-        Car.withGasColorPassengers(6, "Red", "Ender", "Hyrum", "Locke", "Bonzo")
-    );
-    showAll(cars);
-  }
+
+    public static List<Car> getColoredCars(Iterable<Car> in, String color) {
+        List<Car> output = new ArrayList<>();
+
+        for (Car c : in) {
+            if (c.getColor().equals(color)) {
+                output.add(c);
+            }
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        List<Car> cars = Arrays.asList(
+                Car.withGasColorPassengers(6, "Red", "Fred", "Jim", "Sheila"),
+                Car.withGasColorPassengers(3, "Octarine", "Rincewind", "Ridcully"),
+                Car.withGasColorPassengers(9, "Black", "Weatherwax", "Magrat"),
+                Car.withGasColorPassengers(7, "Green", "Valentine", "Gillian", "Anne", "Dr. Mahmoud"),
+                Car.withGasColorPassengers(6, "Red", "Ender", "Hyrum", "Locke", "Bonzo")
+        );
+        showAll(cars);
+        showAll(getColoredCars(cars, "Black"));
+        showAll(cars);
+    }
 }
