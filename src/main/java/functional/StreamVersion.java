3e9b2a1fa231b696256761d92c178addfd5b8e0c
招文桃 <gdouzwt@gmail.com>
2020-03-27 14:08:54 +0800

    whatever

--- src/main/java/functional/StreamVersion.java
+++ src/main/java/functional/StreamVersion.java
@@ -2,24 +2,23 @@ package functional;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class StreamVersion {
 
   public static void main(String[] args) {
     List<String> strings = Arrays.asList(
         "LightCoral", "pink", "Orange", "Gold", "plum", "Blue", "limegreen");
-    
+
     strings
         .stream()
         .forEach(s -> System.out.println(s));
-    
+
     System.out.println("Long: ------------------------");
     strings
         .stream()
         .filter(s -> s.length() > 4)
         .forEach(s -> System.out.println(s));
-    
+
     System.out.println("Original: ------------------------");
     strings
         .stream()
@@ -30,14 +29,14 @@ public class StreamVersion {
         .stream()
         .map(s -> s.toUpperCase())
         .forEach(s -> System.out.println(s));
-    
+
     System.out.println("Short shouting: ------------------------");
     strings
         .stream()
         .filter(s -> s.length() < 6)
         .map(s -> s.toUpperCase())
         .forEach(s -> System.out.println(s));
-    
+
     List<Car> fleet = Arrays.asList(
         Car.withGasColorPassengers(6, "Red", "Fred", "Jim", "Sheila"),
         Car.withGasColorPassengers(3, "Octarine", "Rincewind", "Ridcully"),
@@ -45,7 +44,7 @@ public class StreamVersion {
         Car.withGasColorPassengers(7, "Green", "Valentine", "Gillian", "Anne", "Dr. Mahmoud"),
         Car.withGasColorPassengers(6, "Red", "Ender", "Hyrum", "Locke", "Bonzo")
     );
-    
+
     System.out.println("Drivers with lots of fuel-------------------");
     fleet
         .stream()
@@ -75,7 +74,7 @@ public class StreamVersion {
     fleet
         .stream()
         .filter(c -> c.getGasLevel() > 6)
-        .flatMap(c -> 
+        .flatMap(c ->
             c.getPassengers().stream()
             .map(p -> p + " is in a " + c.getColor() + " car with lots of fuel"))
         .forEach(m -> System.out.println(m));
