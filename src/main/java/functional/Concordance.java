b9019672f7e6f4d243d251be253b292e9720d5ea
招文桃 <gdouzwt@gmail.com>
2020-03-27 15:02:36 +0800

    wrap up

--- src/main/java/functional/Concordance.java
+++ src/main/java/functional/Concordance.java
@@ -1,18 +1,11 @@
 package functional;
 
-import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Concordance {
 
@@ -20,25 +13,15 @@ public class Concordance {
     private static final Comparator<Map.Entry<String, Long>> valueOrder
             = Map.Entry.comparingByValue();
 
-    public static Optional<Stream<String>> lines(Path p) {
-        try {
-            return Optional.of(Files.lines(p));
-        } catch (IOException ioe) {
-            return Optional.empty();
-        }
-    }
-
     public static void main(String[] args) {
-        List<String> filenames = Arrays.asList("PrideAndPrejudice.txt", "Bad.txt", "Emma.txt", "SenseAndSensibility.txt");
+        List<String> filenames =
+                Arrays.asList("PrideAndPrejudice.txt", "Bad.txt", "Emma.txt", "SenseAndSensibility.txt");
         filenames.stream()
                 .map(Paths::get)
-//        .flatMap(Files::lines)
-                .map(Concordance::lines)
-                .peek(s -> {
-                    if (s.isEmpty()) System.err.println("Bad file");
-                })
-                .filter(Optional::isPresent)
-                .flatMap(Optional::get)
+                .map(Either.wrap(Files::lines))
+                .peek(e -> e.handle(System.err::println))
+                .filter(e -> e.success())
+                .flatMap(Either::get)
                 .map(String::toLowerCase)
                 .flatMap(WORD_BREAK::splitAsStream)
                 .filter(s -> s.length() > 0)
